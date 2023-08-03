f66c267e612dbd16651d01c201f55013e06a59c3
Karl J. Overå <k@rl.run>
2023-08-03 15:55:50 +0200

    setup npm deployment

---
+++ packages/npm-diagnosekoder/index.d.ts
@@ -0,0 +1,8 @@
+export type Diagnosekode = {
+  code: string;
+  text: string;
+};
+
+export declare const ICD10: Diagnosekode[];
+
+export declare const ICPC2: Diagnosekode[];
---
+++ packages/npm-diagnosekoder/index.js
@@ -0,0 +1,4 @@
+const ICD10 = require("./ICD10.json");
+const ICPC2 = require("./ICPC2.json");
+
+module.exports = { ICD10, ICPC2 };
