286979e545e6c4a9dbf4ae89e6b2fc3f3aa8e9db
Jostein Stuhaug <jostein.stuhaug@nav.no>
2023-08-09 18:58:38 +0200

    java package version increment automation.
    Introduces github action to increment the version of both javascript and java packages in one workflow, separately from the code generation workflow.

--- java/diagnosekoder/build.gradle.kts
+++ java/diagnosekoder/build.gradle.kts
@@ -5,10 +5,14 @@ val jacksonVersion = "2.15.2"
 
 plugins {
     kotlin("jvm") version "1.9.0"
+    id("maven-publish")
 }
 
 group = "no.nav.helse"
-version = "1.0-SNAPSHOT"
+
+// We use a separate version.txt file to facilitate automatically incrementing/setting the version from github actions.
+val versionFile = rootProject.file("version.txt")
+version = versionFile.readText().trim()
 
 repositories {
     mavenCentral()
@@ -28,7 +32,59 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/diagnosekoder")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                description.set("ICD-10 og ICPC-2 Diagnosekoder fra direktoratet for e-helse.")
+                url.set("https://github.com/navikt/diagnosekoder")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/navikt/diagnosekoder.git")
+                    developerConnection.set("scm:git:https://github.com/navikt/diagnosekoder.git")
+                    url.set("https://github.com/navikt/diagnosekoder")
+                }
+            }
+            from(components["java"])
+        }
+    }
+}
+
 
 tasks.test {
     useJUnitPlatform()
+}
+
+// Used by github action increment-version workflow
+tasks.create("incrementVersion") {
+    group = "my tasks"
+    description = "Increments or sets the version number in the version.txt file."
+    fun generateVersion(): String {
+        val (oldMajor, oldMinor, oldPatch) = version.toString().split(".").map(String::toInt)
+        val (newMajor, newMinor, newPatch) = arrayOf(oldMajor, oldMinor, oldPatch + 1)
+        return "$newMajor.$newMinor.$newPatch"
+    }
+    doLast {
+        val newVersion = properties["overrideVersion"] as String? ?: generateVersion()
+        val oldContent = versionFile.readText()
+        val newContent = oldContent.replace(version.toString(), newVersion)
+        if (oldContent == newContent) {
+            throw Exception("version was not incremented. Could not increment ${oldContent} to ${newContent}")
+        }
+        versionFile.writeText(newContent)
+    }
 }
