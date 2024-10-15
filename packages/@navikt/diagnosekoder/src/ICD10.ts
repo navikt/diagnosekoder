a6289b36ae5a5b703306bf544fa026fab148223d
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-10-15 15:39:09 +0200

    Change import assert to import with.

--- packages/@navikt/diagnosekoder/src/ICD10.ts
+++ packages/@navikt/diagnosekoder/src/ICD10.ts
@@ -1,4 +1,4 @@
-import icd10Json from './ICD10.json' assert {type: 'json'};
+import icd10Json from './ICD10.json' with {type: 'json'};
 import {castToIcd10Diagnosekode, type ICD10Diagnosekode} from "./ICD10Diagnosekode.js";
 
 export const ICD10: ReadonlyArray<ICD10Diagnosekode> = icd10Json.map(castToIcd10Diagnosekode)
